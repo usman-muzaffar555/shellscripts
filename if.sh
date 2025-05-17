7ded34ca5c26cbdcd4a3c16fd3686a7cf12edac7
Usman Muzaffar <umk555@gmail.com>
2025-05-17 12:47:56 +0500

    added

--- if.sh
+++ if.sh
@@ -1,4 +1,26 @@
 # if <,> are being used in if statement, then use double brackts instead of single brackets
+# if pattern matches required like "var" == "var2"* , then use double brackets
+
+# 1. pattern matching
+var="This is test message."
+var2="This is"
+
+if [[ "$var" == "$var2"* ]] ; then
+  echo "This is correct"
+fi
+
+# 2. regex matching
+email="test@example.com"
+
+if [[ $email =~ ^[a-z]+@[a-z]+\.[a-z]+$ ]]; then
+    echo "Valid email"
+fi
+
+# 3. no quotes neede
+if [[ $file == "test" ]]; then 
+    echo "File is test"
+fi
+
 abc="10:10"
 def=10
 for itr in `eval echo {1..10}`
